00e4416393ad3aabcbfeccafffa93353e26d34fd
m <63015852+0mlml@users.noreply.github.com>
2024-04-18 18:20:07 +0300

    code changes

--- app/src/main/java/dev/mlml/ct60a2411project/data/DataFetcher.java
+++ app/src/main/java/dev/mlml/ct60a2411project/data/DataFetcher.java
@@ -1,9 +1,59 @@
 package dev.mlml.ct60a2411project.data;
 
+import java.io.DataInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Objects;
 
-public class DataFetcher {
-    private static void dumpToFile() throws IOException {
+public abstract class DataFetcher {
+    public DataFetcher(URL url) {
+
+    }
+
+    public void update(boolean force) {
+        Path existing = ManifestManager.getExistingDataFile(getClass());
+        if (!force && Objects.nonNull(existing)) {
+            return;
+        }
+
+        File dataFile = null;
+
+        try {
+            String fileName = randomTemporyFileName();
+            File tempFile = downloadData(fileName);
+            dataFile =
+        } catch (IOException e) {
+            if (Objects.isNull(existing)) {
+                throw new RuntimeException("Failed to download data", e);
+            }
+            return;
+        }
+
+        if (Objects.nonNull(dataFile)) {
+            parseData(dataFile);
+            ManifestManager.updateOrCreateEntry(getClass(), dataFile);
+        }
+    }
+
+    private void parseData(Path dataFile) {
+        // Parse the data file
+    }
+
+    private String randomTemporyFileName() {
+        return getClass().getName() + "-" + System.currentTimeMillis();
+    }
+
+    private File downloadData(String fileName) throws IOException {
+        String outputPath = System.getProperty("java.io.tmpdir") + File.separator + fileName;
+        File outputFile = new File(outputPath);
+        outputFile.createNewFile();
+        return outputFile;
+    }
+
+    public record DataEntry(Enum<?> type, String key, String value) {
 
     }
 }
