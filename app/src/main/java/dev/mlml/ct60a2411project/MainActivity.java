fa8922d5526f5d0ba5752490e9ec97f9ef452eab
JUSSIGR <150386346+JUSSIGR@users.noreply.github.com>
2024-04-19 14:20:23 +0300

    search works, new activities and more shit

--- app/src/main/java/dev/mlml/ct60a2411project/MainActivity.java
+++ app/src/main/java/dev/mlml/ct60a2411project/MainActivity.java
@@ -6,16 +6,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 public class MainActivity extends AppCompatActivity {
+
     CardView searchCard;
+    CardView exploreCard;
+    CardView minigamesCard;
+    CardView compareCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         searchCard = findViewById(R.id.searchCard);
+        exploreCard = findViewById(R.id.exploreCard);
+        minigamesCard = findViewById(R.id.minigamesCard);
+        compareCard = findViewById(R.id.compareCard);
+
+
         searchCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        exploreCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ExploreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        minigamesCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MinigamesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        compareCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CompareActivity.class);
                 startActivity(intent);
             }
         });
