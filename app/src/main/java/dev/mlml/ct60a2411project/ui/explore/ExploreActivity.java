83fe2e9aa4e7b91a95ed9b7a2bb13facf13b9863
JUSSIGR <150386346+JUSSIGR@users.noreply.github.com>
2024-04-23 20:57:17 +0300

    new uis

--- app/src/main/java/dev/mlml/ct60a2411project/ui/explore/ExploreActivity.java
+++ app/src/main/java/dev/mlml/ct60a2411project/ui/explore/ExploreActivity.java
@@ -1,36 +1,5 @@
 package dev.mlml.ct60a2411project.ui.explore;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.MaterialAutoCompleteTextView;
-import com.google.android.material.textfield.TextInputLayout;
-
-import java.util.Objects;
-
-import dev.mlml.ct60a2411project.R;
-
 public class ExploreActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_explore);
-
-        TextInputLayout textInputLayout = findViewById(R.id.inputLayout);
-        MaterialAutoCompleteTextView autoCompleteTextView = findViewById(R.id.inputTV);
-        MaterialButton button = findViewById(R.id.btn);
-
-        button.setOnClickListener(view -> {
-            if (Objects.requireNonNull(autoCompleteTextView.getText()).toString().isEmpty()) {
-                textInputLayout.setError("Select an option");
-            } else {
-                Toast.makeText(ExploreActivity.this, autoCompleteTextView.getText().toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 }
